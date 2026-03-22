0a77f37800bb8527621dcf5866d57927ca3173f4
Zayan-Mohamed <itsm.zayan@gmail.com>
2026-03-22 21:32:04 +0530

    feat: This is a testing commit with duplicate button
    Closes: #1
    
    Signed-off-by: Zayan-Mohamed <itsm.zayan@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -21,6 +21,12 @@ function App() {
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
+        <button
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
         <button
           className="counter"
           onClick={() => setCount((count) => count + 1)}
@@ -115,7 +121,7 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
